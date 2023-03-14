a333307872912889c790957b73a29cb09d19133b
austinio7116 <mark.r.austin@gmail.com>
2023-03-14 14:05:52 +0000

    test auto deploy

--- Application/src/RazorPagesTestSample/Program.cs
+++ Application/src/RazorPagesTestSample/Program.cs
@@ -5,6 +5,7 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using RazorPagesTestSample.Data;
+//comment Test for auto deploy
 
 namespace RazorPagesTestSample
 {
