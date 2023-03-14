bfaa7e4a79d91832679d6499b31e0232a2d2d3b1
Leo Siddle @ IXICO <93971692+uizableh@users.noreply.github.com>
2023-03-14 15:12:11 +0000

    Update Program.cs

--- Application/src/RazorPagesTestSample/Program.cs
+++ Application/src/RazorPagesTestSample/Program.cs
@@ -5,7 +5,7 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using RazorPagesTestSample.Data;
-// another update to test ACR
+// another update to test ACR 2
 
 namespace RazorPagesTestSample
 {
